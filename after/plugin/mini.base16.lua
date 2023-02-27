b83b4983582ec0e1f63ea06fa277427ae593c89d
dm <dm@dms-MacBook-Air.local>
2023-02-26 20:47:55 -0800

    descriptions

--- after/plugin/mini.base16.lua
+++ after/plugin/mini.base16.lua
@@ -1,21 +1,21 @@
 require("mini.base16").setup({
 	palette = {
-		base00 = "#191724",
-		base01 = "#1f1d2e",
-		base02 = "#26233a",
-		base03 = "#6e6a86",
-		base04 = "#908caa",
-		base05 = "#e0def4",
-		base06 = "#e0def4",
-		base07 = "#524f67",
-		base08 = "#eb6f92",
-		base09 = "#f6c177",
-		base0A = "#ebbcba",
-		base0B = "#31748f",
-		base0C = "#9ccfd8",
-		base0D = "#c4a7e7",
-		base0E = "#f6c177",
-		base0F = "#524f67",
+		base00 = "#191724", -- Default Background
+		base01 = "#1f1d2e", -- Lighter Background (Used for status bars, line number and folding marks)
+		base02 = "#26233a", -- Selection Background
+		base03 = "#6e6a86", -- Comments, Invisibles, Line Highlighting
+		base04 = "#908caa", -- Dark Foreground (Used for status bars)
+		base05 = "#e0def4", -- Default Foreground, Caret, Delimiters, Operators
+		base06 = "#e0def4", -- Light Foreground (Not often used)
+		base07 = "#524f67", -- Light Background (Not often used)
+		base08 = "#eb6f92", -- Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+		base09 = "#f6c177", -- Integers, Boolean, Constants, XML Attributes, Markup Link Url
+		base0A = "#ebbcba", -- Classes, Markup Bold, Search Text Background
+		base0B = "#31748f", -- Strings, Inherited Class, Markup Code, Diff Inserted
+		base0C = "#9ccfd8", -- Support, Regular Expressions, Escape Characters, Markup Quotes
+		base0D = "#c4a7e7", -- Functions, Methods, Attribute IDs, Headings
+		base0E = "#f6c177", -- Keywords, Storage, Selector, Markup Italic, Diff Changed
+		base0F = "#524f67", -- Deprecated, Opening/Closing Embedded Language Tags
 	},
 	use_cterm = true,
 })
