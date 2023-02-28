1e1cd4f9a6885d1c7775e2b4cc14a5d30b4a33ea
dm <dm@dms-MacBook-Air.local>
2023-02-28 12:31:39 -0800

    :beers:

--- after/plugin/mini.base16.lua
+++ after/plugin/mini.base16.lua
@@ -4,7 +4,7 @@ require("mini.base16").setup({
         base01 = "#1f1d2e", -- Lighter Background (Used for status bars, line number and folding marks)
         base02 = "#26233a", -- Selection Background
 
-        base03 = "#d4d4d4", -- Comments, Invisibles, Line Highlighting
+        base03 = "#585858", -- Comments, Invisibles, Line Highlighting
 
         base04 = "#d4d4d4", -- Dark Foreground (Used for status bars)
         base05 = "#d4d4d4", -- Default Foreground, Caret, Delimiters, Operators
@@ -12,13 +12,13 @@ require("mini.base16").setup({
         base06 = "#d4d4d4", -- Light Foreground (Not often used)
         base07 = "#d4d4d4", -- Light Background (Not often used)
 
-        base08 = "#5fd7ff", -- Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
-        base09 = "#5f87ff", -- Integers, Boolean, Constants, XML Attributes, Markup Link Url
-        base0A = "#afffff", -- Classes, Markup Bold, Search Text Background
-        base0B = "#d7ffd7", -- Strings, Inherited Class, Markup Code, Diff Inserted
-        base0C = "#9a86fd", -- Support, Regular Expressions, Escape Characters, Markup Quotes
+        base08 = "#afd7d7", -- Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+        base09 = "#5fafff", -- Integers, Boolean, Constants, XML Attributes, Markup Link Url
+        base0A = "#00d7af", -- Classes, Markup Bold, Search Text Background
+        base0B = "#5f87af", -- Strings, Inherited Class, Markup Code, Diff Inserted
+        base0C = "#87afff", -- Support, Regular Expressions, Escape Characters, Markup Quotes
         base0D = "#44b8b8", -- Functions, Methods, Attribute IDs, Headings
-        base0E = "#afafff", -- Keywords, Storage, Selector, Markup Italic, Diff Changed
+        base0E = "#87afd7", -- Keywords, Storage, Selector, Markup Italic, Diff Changed
 
         base0F = "#ffffff", -- Deprecated, Opening/Closing Embedded Language Tags
     },
